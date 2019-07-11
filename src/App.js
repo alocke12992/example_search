d57bda0920ace873718d33c4135a20ccc2034d20
Andrew Locke <alocke12992@gmail.com>
2019-07-11 14:51:33 -0700

    Fin

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Search from './Search';
 import './App.css';
 
 function App() {
-  const [query, setQuery] = useState("")
+  const [results, setResults] = useState("")
   const getData = (query) => {
     // DO SOMETHING WITH QUERY - LIKE REQUEST DATA FROM AN API
-    setQuery(query)
+    // I am setting results with the query just to show you how to capture
+    // state from a child component
+    setResults(query)
   }
   return (
     <div className="App">
       <Search getData={getData} />
       <h1>Query</h1>
-      <p>{query}</p>
+      <p>{results}</p>
     </div>
   );
 }
